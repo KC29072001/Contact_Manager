dff403f229158b82652872f325213d96296e0eed
Kartikey <97662990+KC29072001@users.noreply.github.com>
2023-08-29 16:09:40 +0530

    Add files via upload

---
+++ routes/contactRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const {getContacts, createContact, updateContact, deleteContact} = require('../controllers/contactController');
+
+router.route('/').get(getContacts);
+router.route('/').post(createContact);
+router.route('/:id').put(updateContact);
+router.route('/:id').delete(deleteContact);
+module.exports = router;
